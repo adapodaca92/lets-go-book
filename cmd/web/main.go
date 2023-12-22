3b8397b7b40a62af0bc2467b055cdba5e93c911c
Anthony Apodaca <adapodaca92@gmail.com>
2023-12-22 00:01:25 -0800

    Refactored error handling.

---
+++ cmd/web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	logger *slog.Logger
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	logger.Info("Starting server...", "addr", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
+}
